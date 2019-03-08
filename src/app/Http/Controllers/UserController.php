dc7b18976b007d1d0b434f5f7f8af6c630e36d2f
tomi <15144450+tmxdev@users.noreply.github.com>
2019-03-08 16:38:56 +0900

    Simplify UserController

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -13,17 +13,25 @@ class UserController extends Controller
   public function top($github_id, Request $request)
   {
     $status = LoginController::is_user_loggedin($request);
-    $status->put('name', $github_id);
 
-    $avater = User::where('github_id', $github_id)->value('avaterfile');
+    // ユーザの存在確認
+    if (User::where('github_id', $github_id)->exists()) {
+      $thisuser = User::where('github_id', $github_id);
+    } else {
+      return redirect('/home');
+    }
+
+    $status->put('name', $github_id);
+    $avater = $thisuser->value('avaterfile');
     $status->put('avater', $avater);
 
-    $thisuserid = User::where('github_id', $github_id)->value('id');
-    $thisuserposts = Post::latest('created_at')->where('user_id', $thisuserid)->get();
+    // 表示ユーザの全投稿取得
+    $thisuserposts = Post::latest('created_at')->where('user_id', $thisuser->value('id'))->get();
     $status->put('thisuserposts', $thisuserposts);
-
-    $thisuserlikes = User::where('id', $thisuserid)->get()->first()->likePosts()->count();
+    // 表示ユーザのLike数取得
+    $thisuserlikes = $thisuser->get()->first()->likePosts()->count();
     $status->put('thisuserlikes', $thisuserlikes);
+
     return view('/user', $status->all());
   }
 }
